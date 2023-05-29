49ebdcebe13b0609e08a529c3d546e10105b84ea
Deepak Rathod <103870640+rathoddeepak@users.noreply.github.com>
2023-05-29 16:03:11 +0530

    Update 2. Valid Anagram.js

--- Array and Hashing/2. Valid Anagram.js
+++ Array and Hashing/2. Valid Anagram.js
@@ -1,1 +1,27 @@
-ok
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if(s.length !== t.length){
+      return false;
+  }
+  const mapS = {};
+  const mapT = {};
+  for(let i = 0; i < s.length; i++){
+      if(mapS[s[i]])mapS[s[i]] += 1;
+      else mapS[s[i]] = 1;
+      
+      if(mapT[t[i]])mapT[t[i]] += 1;
+      else mapT[t[i]] = 1;
+  }
+
+  for(let x of s){
+      if(mapS[x] !== mapT[x]){
+          return false;
+      }
+  }
+    
+  return true;
+};
